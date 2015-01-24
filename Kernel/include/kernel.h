5f147d4cf3adfcb6a774f8660f46ecba3734514e
Will Sowerbutts <will@sowerbutts.com>
2015-01-24 23:58:41 +0000

    Revert "need_resched(): reschedule polling processes after only one tick"
    This reverts commit dad8151f7d2b306a0e886b295dc83012d1b22fc5.

--- Kernel/include/kernel.h
+++ Kernel/include/kernel.h
@@ -705,7 +705,7 @@ CODE2 void timer_interrupt(void);
 CODE2 void doexit (int16_t val, int16_t val2);
 CODE2 void panic(char *deathcry);
 CODE2 void exec_or_die(void);
-#define need_resched() (nready != 1 && runticks)
+#define need_resched() (nready != 1 && runticks >= udata.u_ptab->p_priority)
 
 
 /* select.c */
