2fe6f6939f2b010b333f51bdf092037fcca47f35
Will Sowerbutts <will@sowerbutts.com>
2015-01-25 23:48:23 +0000

    Revert "devio: Remove safety check"
    This reverts commit 274d9a6307447708230e0f847a6d1c9b394fd0b3.

--- Kernel/devio.c
+++ Kernel/devio.c
@@ -34,6 +34,8 @@ uint8_t *bread(uint16_t dev, blkno_t blk, bool rewrite)
 	register bufptr bp;
 
 	if ((bp = bfind(dev, blk)) != NULL) {
+		if (bp->bf_busy)
+			panic("want busy block");
 		goto done;
 	}
 	bp = freebuf();
