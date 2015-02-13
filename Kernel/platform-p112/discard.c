fc86047f2937ede3a4359845943a2d13500afc26
Will Sowerbutts <will@sowerbutts.com>
2015-02-13 20:41:33 +0000

    p112, n8vem-mark4: Better root device parsing

--- Kernel/platform-p112/discard.c
+++ Kernel/platform-p112/discard.c
@@ -2,19 +2,23 @@
 #include <kdata.h>
 #include <printf.h>
 #include <devtty.h>
-#include "config.h"
 #include <z180.h>
+#include "config.h"
+#ifdef CONFIG_P112_FLOPPY
+#include "devfd.h"
+#endif
 
 void pagemap_init(void)
 {
     int i;
 
-    /* N8VEM SBC Mark IV has RAM in the top 512K of physical memory. 
+    /* P112 has RAM across the full physical 1MB address space
      * First 64K is used by the kernel. 
      * Each process gets the full 64K for now.
      * Page size is 4KB. */
-    for(i = 0x90; i < 0x100; i+=0x10)
+    for(i = 0x10; i < 0x100; i+=0x10){
         pagemap_add(i);
+    }
 }
 
 void map_init(void)
